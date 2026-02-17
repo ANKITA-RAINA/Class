efb57b7ffb7de7ed75ef679b25f32ca5b47c161b
ANKITA-RAINA <ankitaraina023@gmail.com>
2026-02-17 11:54:54 +0530

    Linear Regression

--- Project.py
+++ Project.py
@@ -1,1 +1,14 @@
 print("hello")
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+data=pd.DataFrame({
+    "Experience":[1,2,3,4,5,6,7,8],
+    "Salary":[30000,35000,40000,45000,5000,55000,60000,65000]
+})
+
+X=data[["Experience"]]
+y=data["Salary"]
+model=LinearRegression()
+model.fit(X,y)
+joblib.dump(model,"salary_model.pkl")
