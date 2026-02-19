c33c3ff6c624032adfcdf333bf2ee50228892040
ANKITA-RAINA <ankitaraina023@gmail.com>
2026-02-19 11:46:18 +0530

    Calculator

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import streamlit as st
-import joblib
-import numpy as np
-st.write("Streamlit app")
-model=joblib.load('salary_model.pkl')
-Experience=st.slider("Years of experience",min_value=0.0,max_value=15.0,step=0.5)
-prediction=model.predict([[Experience]])
-st.subheader("Predicted Salary")
-st.write(f"${prediction[0]:,.2f}")
+st.title("Calculator")
+n1=st.number_input("Enter the first number")
+n2=st.number_input("Enter the second number")
+operator=st.text_input("Enter the operator")
 
+if operator=='+':
+    st.write(n1+n2)
+elif operator =='-':
+    st.write(n1-n2)
+elif operator =='*':
+    st.write(n1*n2)
+elif operator =='/':
+    st.write(n1/n2)
+elif operator =='**':
+    st.write(n1**n2)
+elif operator =='//':
+    st.write(n1//n2)
+else:
+    st.write("Enter correct operator")
 
